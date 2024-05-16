890b9e775da3e5b3135ede83793d17f62c75c891
Diana122807 <dianacarolinaramirrezperez@gmail.com>
2024-05-16 15:26:19 -0500

    Código stranger things
    Creación y solución código stranger things

---
+++ Programa strange things.cs
@@ -0,0 +1,18 @@
+/******************************************************************************
+ autores: Santiago alvarez, diana carolina 
+ fecha: 16.05.24
+  los personajes de stranger things necesitan un programa de C# para registrar 
+  los encuentros con criaturas del mundo como el Demogorgon. Completa el espacio
+  en blanco para imprimir el nombre de la criatura en la consola. 
+
+*******************************************************************************/
+using System;
+class Programa 
+  {
+      
+  static void Main( string[] args ) 
+    {
+        string nombreCriatura = "Demogorgon";
+        Console.WriteLine("el nombre de la Criatura es " + nombreCriatura);
+  }
+}
