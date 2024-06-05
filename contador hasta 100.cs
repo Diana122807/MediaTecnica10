1c0bd0a1bae44254868a4817db18dd65568d3ec9
Diana122807 <dianacarolinaramirrezperez@gmail.com>
2024-06-05 13:41:53 -0500

    Add files via upload

---
+++ contador hasta 100.cs
@@ -0,0 +1,15 @@
+/******************************************************************************
+Autora: Diana Carolina Ramirez
+Fecha:05-06-24
+Descripcion: Programa aprendizaje ciclos o bucles
+                 Contador hasta 100
+********************************************************************************/
+using System;
+class HelloWorld {
+  static void Main() {
+      for (int i = 0; i <= 100; i++) 
+    {
+        Console.WriteLine("El valor actual es: " + i);
+    }
+  }
+}
